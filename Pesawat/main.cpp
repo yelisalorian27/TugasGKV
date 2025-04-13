559e319060014722f70a8ace8dd36f1a42300337
yelisalorian27 <yelisalorian04@gmail.com>
2025-04-13 11:07:21 +0700

    Initial Commit

---
+++ Pesawat/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pesawat
+//
+//  Created by Yelisa Lorian on 13/04/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
